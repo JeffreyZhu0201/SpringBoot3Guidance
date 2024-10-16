f52e3aed0f218b6d5a8c9dad4ec76e794cefdd91
JeffreyZhu <1624410543@qq.com>
2024-10-17 00:37:10 +0800

    Initialize the project and added the "Create project" period

---
+++ src/main/java/com/example/springboot3guidance/SpringBoot3GuidanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3guidance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3GuidanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3GuidanceApplication.class, args);
+    }
+
+}
